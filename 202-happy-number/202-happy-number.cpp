afb9ccb8bfb45256254c60843fdbac6934fdfffd
Dhaval Vaniya <91320236+dhaval3800@users.noreply.github.com>
2022-07-29 16:52:26 +0530

    Time: 4 ms (46.13%), Space: 6.4 MB (25.73%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     bool isHappy(int n) {
-                 
-    set<int> s;
-    while(s.count(n)==0)
-    {
-        if(n==1) return true;
-        s.insert(n);
-        int temp=0;
-        while(n)
-        {
-            temp+= (n%10)*(n%10);
-            n/=10;
+        set<int> myset;
+        int val;
+        int index;
+        while(myset.count(n)==0){
+            if(val==1) return true;
+            myset.insert(val);
+            val= 0;
+            while(n){
+                index = n%10;
+                val += index*index;
+                n = n/10;
+            }
+            n = val;
         }
-        n=temp;
+        return false;
     }
-    return false;
-}
 };
