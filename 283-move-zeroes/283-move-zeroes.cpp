ad6d2f7df59c3510c1411e13d921d9dc03c5a379
Dhaval Vaniya <91320236+dhaval3800@users.noreply.github.com>
2022-08-09 10:54:00 +0530

    Time: 53 ms (20.08%), Space: 19.4 MB (13.34%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -9,27 +9,9 @@ public:
             }
         }
         
-        while(j<nums.size()){
+        for(;j<nums.size();j++){
             nums[j]=0;
-            j++;
         }
         
         }
 };
-
-
-
-//        for(int i=0; i<nums.size(); i++){
-//             if(nums[i]!=0){
-//                 continue;
-//             }
-//             for(int j = i+1; j<nums.size();j++){
-//                 if(nums[j]==0){
-//                     continue;                    
-//                 }
-//                 else{
-//                     swap(nums[i],nums[j]);
-//                     break;
-//                 }
-//             }
-//         }
