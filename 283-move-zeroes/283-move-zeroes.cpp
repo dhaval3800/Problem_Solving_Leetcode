ca3f36dddce9a519d425b2d5b690f13401d95582
Dhaval Vaniya <91320236+dhaval3800@users.noreply.github.com>
2022-08-11 00:59:18 +0530

    Time: 33 ms (65.67%), Space: 19.3 MB (13.34%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -4,31 +4,28 @@ public:
         int j = 0;
         for(int i =0; i<nums.size();i++){
             if(nums[i]!=0){
-                nums[j]=nums[i];
+                swap(nums[j],nums[i]);
                 j++;
             }
         }
-        
-        for(;j<nums.size();j++){
-            nums[j]=0;
-        }
-        
-        }
+    }
 };
 
 
 
-//        for(int i=0; i<nums.size(); i++){
+    
+
+//         void moveZeroes(vector<int>& nums) {
+//         int j = 0;
+//         for(int i =0; i<nums.size();i++){
 //             if(nums[i]!=0){
-//                 continue;
+//                 nums[j]=nums[i];
+//                 j++;
 //             }
-//             for(int j = i+1; j<nums.size();j++){
-//                 if(nums[j]==0){
-//                     continue;                    
-//                 }
-//                 else{
-//                     swap(nums[i],nums[j]);
-//                     break;
-//                 }
-//             }
+//         }
+        
+//         for(;j<nums.size();j++){
+//             nums[j]=0;
+//         }
+        
 //         }
