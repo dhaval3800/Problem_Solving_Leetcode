f55da518d40829e3eab0bdbf78c5b4ec8b2beeaa
Dhaval Vaniya <91320236+dhaval3800@users.noreply.github.com>
2022-08-25 19:14:35 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (78.79%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,26 @@
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if(n==0) return 0;
+//         if(n==1) return 1;        
+//         return fib(n-1)+ fib(n-2);
+//     }
+// };
+
+
 class Solution {
 public:
     int fib(int n) {
+        int a = 0;
+        int b =1;
+        int ans = 0;
         if(n==0) return 0;
-        if(n==1) return 1;        
-        return fib(n-1)+ fib(n-2);
+        if(n==1) return 1;
+        for(int i = 2; i<=n; i++){
+            ans = a+b;
+            a = b;
+            b = ans;
+        }
+        return ans;
     }
 };
