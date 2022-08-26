bda8d3f66d0c91c78ad595f532800220d8a5cc78
Dhaval Vaniya <91320236+dhaval3800@users.noreply.github.com>
2022-08-26 21:34:41 +0530

    Time: 235 ms (23.83%), Space: 39.2 MB (26.04%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,103 +1,40 @@
-// class Solution {
-// public:
-//     vector<int> sortArray(vector<int>& nums) {
-//         int n = nums.size();
-        
-//         for(int i=0;i<n-1; i++)
-//         {
-//             bool swapped = false;
-//             for(int j=0; j<n-i-1; j++)
-//             {
-//                 if(nums[j]> nums[j+1])
-//                 {
-//                     swap(nums[j+1],nums[j]);
-//                     swapped = true;
-//                 }
-//             }
-//             if(swapped == false){
-//                 break;
-//             }
-//         }
-//         return nums;
-//     }        
-// };
-
-
 class Solution {
-    
-
-    void mergeSort(vector<int>& arr, int s, int e) {
-
-        //base case
-        if(s >= e) {
-            return;
-        }
-    
-        int mid = (s+e)/2;
-
-    //left part sort karna h 
-        mergeSort(arr, s, mid);
-    
-    //right part sort karna h 
-        mergeSort(arr, mid+1, e);
-
-    //merge
-        merge(arr, s, e);
-
-    }
-    
-    void merge(vector<int>& arr, int s, int e) {
-        
-        int mid = (s+e)/2;
-
-        int len1 = mid - s + 1;
-        int len2 = e - mid;
-
-        int *first = new int[len1];
-        int *second = new int[len2];
-
-        //copy values
-        int mainArrayIndex = s;
-        for(int i=0; i<len1; i++) {
-            first[i] = arr[mainArrayIndex++];
-        }
-
-        mainArrayIndex = mid+1;
-        for(int i=0; i<len2; i++) {
-            second[i] = arr[mainArrayIndex++];
-        }
-
-    //merge 2 sorted arrays     
-        int index1 = 0;
-        int index2 = 0;
-        mainArrayIndex = s;
-
-        while(index1 < len1 && index2 < len2) {
-            if(first[index1] < second[index2]) {
-                arr[mainArrayIndex++] = first[index1++];
-            }
-            else{
-                arr[mainArrayIndex++] = second[index2++];
-            }
-        }   
-
-        while(index1 < len1) {
-            arr[mainArrayIndex++] = first[index1++];
-        }
-
-        while(index2 < len2 ) {
-            arr[mainArrayIndex++] = second[index2++];
-        }
-
-        delete []first;
-        delete []second;
-
-    }
-    
 public:
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size()-1);
-        return nums;
+      mergeSort(nums, 0, nums.size() - 1);
+      return nums;
     }
-    
+    void mergeSort(vector<int> &arr, int low, int high) {
+      if (low < high) {
+        int mid = (high + low) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+      }
+    }
+    void merge(vector<int> &arr, int low, int mid, int high) {
+      int n1 = mid - low + 1;
+      int n2 = high - mid;
+      int left[n1], right[n2];
+      for (int i = 0; i < n1; i++) {
+        left[i] = arr[low + i];
+      }
+      for (int i = 0; i < n2; i++) {
+        right[i] = arr[mid + 1 + i];
+      }
+      int i = 0, j = 0, k = low;
+      while (i < n1 && j < n2) {
+        if (left[i] < right[j]) {
+          arr[k++] = left[i++];
+        } else {
+          arr[k++] = right[j++];
+        }
+      }
+      while (i < n1) {
+        arr[k++] = left[i++];
+      }
+      while (j < n2) {
+        arr[k++] = right[j++];
+      }
+    }
 };
