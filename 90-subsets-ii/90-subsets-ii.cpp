df4086efa056951a6d810627aef35857445550c7
Dhaval Vaniya <91320236+dhaval3800@users.noreply.github.com>
2022-08-27 09:56:36 +0530

    Time: 23 ms (7.37%), Space: 18.5 MB (5.48%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,41 +1,33 @@
 class Solution {
-public:
-    
-    void solve(vector<int> nums, int index, vector<int> output, set<vector<int>> &temp)
-    {
-        if(index >= nums.size())
-        {
+ 
+    void SubSet(vector<int> nums, vector<int> output, int index, set<vector<int>> &temp){
+        //base case
+        if(index >= nums.size()){
             temp.insert(output);
             return;
         }
         
-        solve(nums, index+1, output, temp);
+        //exclude
+        SubSet(nums, output, index+1, temp);
         
+        //include
         output.push_back(nums[index]);
-        
-        solve(nums, index+1, output, temp);
+        SubSet(nums, output, index+1,temp);
     }
     
+public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        
         sort(nums.begin(), nums.end());
-        
         vector<vector<int>> ans;
-        
-        set<vector<int>> temp;
-        
         vector<int> output;
-        
+        set<vector<int>> temp;
         int index = 0;
+        SubSet(nums, output,  index,temp);       
         
-        solve(nums, index, output, temp);
-        
-        for(auto i:temp)
-        {
-           ans.push_back(i);
+        for(auto i: temp){
+            ans.push_back(i);
         }
-        
         return ans;
-        
     }
+
 };
