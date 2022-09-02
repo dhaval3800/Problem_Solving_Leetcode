f06c17fd3ab20a3d563cbc5b20f466d31724d1fc
Dhaval Vaniya <91320236+dhaval3800@users.noreply.github.com>
2022-09-02 16:50:01 +0530

    Time: 26 ms (12.28%), Space: 10 MB (8.78%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -6,40 +6,67 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+//floyd cycle detection (t(c)=O(n) & s(c)= O(1)) 
+
+// class Solution {
+// public:
+    
+//     ListNode *detectCycle(ListNode *head) {
+//         if(head==NULL) return NULL;
+        
+//         ListNode* intersection = hasCycle(head);
+//         if(intersection==NULL) return NULL;
+//         ListNode* slow = head;
+//         int cnt = -1;
+//         while(slow!=intersection){
+//             slow = slow->next;
+//             intersection = intersection->next;
+//             cnt++;
+//         }
+//         return slow;
+//     }
+    
+    
+//     ListNode* hasCycle(ListNode *head) {
+//         if(head==NULL) return NULL;
+        
+//         ListNode* slow = head;
+//         ListNode* fast = head;
+        
+//         while(fast!=NULL && slow!=NULL){
+//             slow = slow->next;
+//             fast = fast->next;
+//             if(fast!=NULL){
+//                 fast = fast->next;
+//             }
+//             if(slow==fast) return slow;
+//         }
+//         return NULL;
+//     }
+    
+// };
+
+
+
+//using Map (t(c)=O(n) & s(c)= O(n)) 
+
 class Solution {
 public:
     
-    ListNode *detectCycle(ListNode *head) {
-        if(head==NULL) return NULL;
-        
-        ListNode* intersection = hasCycle(head);
-        if(intersection==NULL) return NULL;
-        ListNode* slow = head;
-        int cnt = -1;
-        while(slow!=intersection){
-            slow = slow->next;
-            intersection = intersection->next;
-            cnt++;
-        }
-        return slow;
-    }
-    
-    
-    ListNode* hasCycle(ListNode *head) {
-        if(head==NULL) return NULL;
-        
-        ListNode* slow = head;
-        ListNode* fast = head;
-        
-        while(fast!=NULL && slow!=NULL){
-            slow = slow->next;
-            fast = fast->next;
-            if(fast!=NULL){
-                fast = fast->next;
+    ListNode* detectCycle(ListNode *head) {
+        map<ListNode* , bool> visited;
+        ListNode* temp = head;
+        while(temp!=NULL){
+            if(visited[temp]==true){
+                return temp;
             }
-            if(slow==fast) return slow;
+            else{
+                visited[temp]= true;
+            }
+            temp = temp->next;
         }
         return NULL;
     }
-    
 };
+
