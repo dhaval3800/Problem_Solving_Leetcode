d3bee682e2ffbe890c18445771f462496a570a79
Dhaval Vaniya <91320236+dhaval3800@users.noreply.github.com>
2022-09-02 15:52:27 +0530

    Time: 25 ms (19.76%), Space: 8.1 MB (17.76%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -6,20 +6,43 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+//Using Map
+
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         map<ListNode* , bool> visited;
+//         ListNode* temp = head;
+//         while(temp!=NULL){
+//             if(visited[temp]==true){
+//                 return true;
+//             }
+//             else{
+//                 visited[temp]= true;
+//             }
+//             temp = temp->next;
+//         }
+//         return false;
+//     }
+// };
+
+
+//Using Floyd Cycle detection
+
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        map<ListNode* , bool> visited;
-        ListNode* temp = head;
-        while(temp!=NULL){
-            if(visited[temp]==true){
-                return true;
-            }
-            else{
-                visited[temp]= true;
-            }
-            temp = temp->next;
+        if(head==NULL) return 0;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast) return 1;
         }
-        return false;
+        return 0;
     }
 };
