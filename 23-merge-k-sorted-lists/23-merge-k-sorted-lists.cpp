d84a9fe57137466e6b2b04ccafee0cfcf9e0612f
Dhaval Vaniya <91320236+dhaval3800@users.noreply.github.com>
2022-09-08 01:40:03 +0530

    Time: 58 ms (35.94%), Space: 14.1 MB (11.35%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -8,70 +8,101 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* mergeKLists(vector<ListNode*>& lists) {
+//         if (lists.size() == 0) return NULL;
+        
+//         ListNode* head = lists[0];
+        
+//         for (int i = 1; i < lists.size(); i++)
+//             head = mergeTwoLists(head, lists[i]);
+        
+//         return head;
+//     }
+    
+// //======================================================================//
+    
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         if(list1==NULL) return list2;
+//         if(list2==NULL) return list1;
+        
+        
+//         if(list1->val <= list2->val){
+//             return solve(list1, list2);
+//         }
+//         else{
+//             return solve(list2,list1);
+//         }
+//     }
+    
+// //======================================================================//
+    
+//     ListNode* solve(ListNode* list1 , ListNode* list2){
+        
+//         ListNode* temp1 = list1;
+//         ListNode* temp2 = list2;
+//         ListNode* next1 = temp1->next;
+//         ListNode* next2 = temp2->next;
+        
+//         if(next1==NULL && temp2!=NULL ){
+//             temp1->next = temp2;
+//             return list1;
+//         }
+        
+//         while(next1!= NULL && temp2!=NULL){
+            
+//             if((temp1->val <= temp2->val) && (temp2->val <=next1->val) ){
+//                 temp1->next = temp2;
+//                 next2 = temp2->next;
+//                 temp2->next = next1;   
+                
+//                 temp1 = temp2;
+//                 temp2 = next2;  
+//             }
+            
+//             else {
+//                 temp1 = next1;
+//                 next1 = next1->next;
+                
+//                 if(next1==NULL){
+//                     temp1->next = temp2;
+//                     return list1;
+//                 }
+//             }
+            
+//         }
+//         return list1;
+//     }
+// };
+
+
+
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.size() == 0) return NULL;
+        map<int, int> nodes;
         
-        ListNode* head = lists[0];
-        
-        for (int i = 1; i < lists.size(); i++)
-            head = mergeTwoLists(head, lists[i]);
-        
-        return head;
-    }
-    
-//======================================================================//
-    
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL) return list2;
-        if(list2==NULL) return list1;
-        
-        
-        if(list1->val <= list2->val){
-            return solve(list1, list2);
-        }
-        else{
-            return solve(list2,list1);
-        }
-    }
-    
-//======================================================================//
-    
-    ListNode* solve(ListNode* list1 , ListNode* list2){
-        
-        ListNode* temp1 = list1;
-        ListNode* temp2 = list2;
-        ListNode* next1 = temp1->next;
-        ListNode* next2 = temp2->next;
-        
-        if(next1==NULL && temp2!=NULL ){
-            temp1->next = temp2;
-            return list1;
-        }
-        
-        while(next1!= NULL && temp2!=NULL){
-            
-            if((temp1->val <= temp2->val) && (temp2->val <=next1->val) ){
-                temp1->next = temp2;
-                next2 = temp2->next;
-                temp2->next = next1;   
-                
-                temp1 = temp2;
-                temp2 = next2;  
+        for(int i =0; i<lists.size(); i++){
+            ListNode* node = lists[i];
+            while(node){
+                nodes[node->val]++;
+                node = node->next;
             }
-            
-            else {
-                temp1 = next1;
-                next1 = next1->next;
-                
-                if(next1==NULL){
-                    temp1->next = temp2;
-                    return list1;
-                }
-            }
-            
         }
-        return list1;
+        
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* tail = dummyNode;
+        
+        for(auto a:nodes){
+            while(a.second!=0){
+                ListNode* newNode = new ListNode(a.first);
+                tail->next = newNode;
+                tail = tail->next;
+                a.second--;
+            }
+        }
+        return dummyNode->next;
     }
+        
 };
