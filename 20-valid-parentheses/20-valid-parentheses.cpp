14a041b01cc5f288dcd2530cdc2b51ba455164b2
Dhaval Vaniya <91320236+dhaval3800@users.noreply.github.com>
2022-09-13 10:26:11 +0530

    Time: 9 ms (5.55%), Space: 6.4 MB (50.99%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,26 +1,62 @@
+// class Solution {
+// public:
+//     bool isValid(string s) {
+        
+//         stack<char> st;
+        
+//         if(s[0]=='}' || s[0]==')' || s[0]==']') 
+//             return false;
+//         else
+//             st.push(s[0]);
+
+//         for(int i =1; i<s.size(); i++){  
+            
+//             if(!st.empty() && s[i]==')' && st.top()=='(')
+//                 st.pop();
+//             else if(!st.empty() && s[i]==']'&& st.top()=='[') 
+//                 st.pop();
+//             else if(!st.empty() && s[i]=='}'&& st.top()=='{') 
+//                 st.pop();
+//             else 
+//                 st.push(s[i]);
+
+//         }
+//         if(st.empty()){
+//             return true;
+//         }
+//         else
+//             return false;
+//     }
+// };
+
+
 class Solution {
 public:
     bool isValid(string s) {
         
         stack<char> st;
         
-        if(s[0]=='}' || s[0]==')' || s[0]==']') 
-            return false;
-        else
-            st.push(s[0]);
-
-        for(int i =1; i<s.size(); i++){  
-            
-            if(!st.empty() && s[i]==')' && st.top()=='(')
-                st.pop();
-            else if(!st.empty() && s[i]==']'&& st.top()=='[') 
-                st.pop();
-            else if(!st.empty() && s[i]=='}'&& st.top()=='{') 
-                st.pop();
-            else 
+        for(int i =0; i<s.size(); i++){  
+              
+            //for opening brackets
+            if(s[i]=='{' || s[i]=='(' || s[i]=='[') 
                 st.push(s[i]);
-
+            else
+                //for closing brackets
+                if(!st.empty()){
+                    if((s[i]==')' && st.top()=='(') || (s[i]==']'&& st.top()=='[') || 
+                       (s[i]=='}'&& st.top()=='{')){
+                        cout<< st.top();
+                        st.pop();
+                    }
+                    else
+                        return false;
+                }
+                else 
+                    return false;
+               
         }
+        
         if(st.empty()){
             return true;
         }
@@ -28,3 +64,5 @@ public:
             return false;
     }
 };
+
+
